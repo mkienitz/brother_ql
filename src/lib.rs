9c913e45c2060b08cf2dcc65bab85737ce736d5e
Maximilian Kienitz <max@kienitz.dev>
2024-01-27 21:40:50 +0100

    chore: adjust visibility of library modules, hiding technical details

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,5 @@
-pub mod commands;
-pub mod media;
-pub mod job;
+mod commands;
 pub mod error;
-pub mod image;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod image;
+pub mod job;
+pub mod media;
