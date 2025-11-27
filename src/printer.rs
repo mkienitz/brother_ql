9a4c06276fc99cdd6513476455b4c214d44ab7f9
Adrian Moennich <adrian@planetcoding.net>
2025-11-27 23:08:39 +0100

    Add QL700

--- src/printer.rs
+++ src/printer.rs
@@ -5,6 +5,8 @@ use crate::error::StatusParsingError;
 /// Brother QL printer model
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum PrinterModel {
+    /// QL-700
+    QL700,
     /// QL-800
     QL800,
     /// QL-810W
@@ -16,6 +18,7 @@ pub enum PrinterModel {
 impl PrinterModel {
     pub(crate) const fn pid(&self) -> u16 {
         match self {
+            PrinterModel::QL700 => 0x2042,
             PrinterModel::QL800 => 0x209b,
             PrinterModel::QL810W => 0x209c,
             PrinterModel::QL820NWB => 0x209d,
@@ -28,6 +31,7 @@ impl TryFrom<u8> for PrinterModel {
 
     fn try_from(value: u8) -> Result<Self, Self::Error> {
         match value {
+            0x35 => Ok(Self::QL700),
             0x38 => Ok(Self::QL800),
             0x39 => Ok(Self::QL810W),
             0x41 => Ok(Self::QL820NWB),
